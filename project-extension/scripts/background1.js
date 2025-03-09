bbb4d32793cdf46c8af0a24da84054696dfb3397
MounikaRadha <radha.mounika@testsigma.com>
2025-03-09 22:37:41 +0530

    feat/urls for which pwd is typed will not be opened

--- project-extension/scripts/background1.js
+++ project-extension/scripts/background1.js
@@ -1,10 +1,10 @@
 const tabHandling = async (tab) => {
-  let urls = await chrome.storage.sync.get("key1");
-  console.log("urls are" + urls);
-  let url = await chrome.storage.sync.get(null,(items)=>{
+  let url = await chrome.storage.sync.get(tab.url);
+  let urls = await chrome.storage.sync.get(null,(items)=>{
     console.log("items"+items)
   });
-  if (tab.title == "New Tab" || chrome.runtime.lastError) return;
+  let notSecured=Object.keys(url).length==0
+  if (tab.title == "New Tab" || chrome.runtime.lastError ||notSecured) return;
     chrome.tabs.sendMessage(tab.id, { data: "test" }, (response) => {
       if (chrome.runtime.lastError) {
         console.log("err--", chrome.runtime.lastError + "with tab as" + tab);
