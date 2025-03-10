9419d2d31d5d13d64f889e5467acdd6b981c7441
MounikaRadha <radha.mounika@testsigma.com>
2025-03-10 08:34:26 +0530

    feat/if site is protected dont allow to give another password

--- project-extension/scripts/popup.js
+++ project-extension/scripts/popup.js
@@ -1,21 +1,26 @@
 const closePopup = () => {
   window.close();
 };
-const sumbitFormHandler = async(event) => {
-  document.getElementById("successDiv").style.display = "block";
-  document.getElementById("passwordDiv").style.display = "none";
+const sumbitFormHandler = async (event) => {
   const password = document.getElementById("passwordField").value;
   var query = { active: true, currentWindow: true };
-  chrome.tabs.query(query, async(tabs) => {
-    let url1=tabs[0].url
-     await chrome.storage.sync.set({
-       [url1]: password,
+  chrome.tabs.query(query, async (tabs) => {
+    let url1 = tabs[0].url;
+    const obj = await chrome.storage.sync.get([url1]);
+    let notSecured = Object.keys(obj).length == 0;
+    if (notSecured) {
+      await chrome.storage.sync.set({
+        [url1]: password,
       });
-    });
+      document.getElementById("successDiv").style.display = "block";
+      document.getElementById("passwordDiv").style.display = "none";
+    } else {
+      document.getElementById("alreadyProtected").style.display = "block";
+    }
+  });
 };
 
 document
   .getElementById("submitPassword")
   .addEventListener("click", sumbitFormHandler);
 document.getElementById("closeWindow").addEventListener("click", closePopup);
-
