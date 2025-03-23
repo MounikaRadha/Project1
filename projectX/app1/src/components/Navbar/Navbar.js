f04c9b1254f035d5272a00eeb77f22ae7ec669db
MounikaRadha <radha.mounika@testsigma.com>
2025-03-23 12:49:33 +0530

    feat/navbar css added

--- projectX/app1/src/components/Navbar/Navbar.js
+++ projectX/app1/src/components/Navbar/Navbar.js
@@ -1,14 +1,17 @@
 import URL_Constants from "../../constants/Url_Constants";
+import "./Navbar.css"; // Import CSS file
 
-const Navbar=()=>{
-    return(
-        <>
-       <a href={URL_Constants.BASE_NAME+URL_Constants.USERS_PATH}> users </a>
-        <a href={URL_Constants.BASE_NAME+URL_Constants.END_POINTS_PATH}>endpoint </a>
-        <a href={URL_Constants.BASE_NAME+URL_Constants.POSTED_DATA_PATH}>postedData</a>
-        <a href={URL_Constants.BASE_NAME+"/counter"}> counter</a>
-        <a href={URL_Constants.BASE_NAME}>home</a>
-        </>
-    )
-}
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <a href={URL_Constants.BASE_NAME} className="nav-link">Home</a>
+            <a href={URL_Constants.BASE_NAME + URL_Constants.USERS_PATH} className="nav-link">Users</a>
+            <a href={URL_Constants.BASE_NAME + URL_Constants.END_POINTS_PATH} className="nav-link">Endpoint</a>
+            <a href={URL_Constants.BASE_NAME + URL_Constants.POSTED_DATA_PATH} className="nav-link">Posted Data</a>
+            <a href={URL_Constants.BASE_NAME + "/counter"} className="nav-link">Counter</a>
+            
+        </nav>
+    );
+};
+
 export default Navbar;
