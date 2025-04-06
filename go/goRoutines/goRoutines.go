a1654a8dc2e24d0f8e479728be7e1b8715b35354
MounikaRadha <radha.mounika@testsigma.com>
2025-04-06 23:57:39 +0530

    feat/sample go routine practice

--- go/goRoutines/goRoutines.go
+++ go/goRoutines/goRoutines.go
@@ -1,5 +1,14 @@
 package main
 import "fmt"
+import "sync"
+func printHi(wg *sync.WaitGroup){
+	defer wg.Done()
+	fmt.Print("hi from printt \n")
+}
 func main(){
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go printHi(&wg)
+	wg.Wait()
 fmt.Print("in main func of go routine test")
 }
