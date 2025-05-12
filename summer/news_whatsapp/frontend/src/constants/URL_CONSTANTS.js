2a3d81b25471425fa46373050075a52648017ef0
MounikaRadha <radha.mounika@testsigma.com>
2025-05-12 20:33:27 +0530

    feat/newsScraping protocol of backend url updated

--- summer/news_whatsapp/frontend/src/constants/URL_CONSTANTS.js
+++ summer/news_whatsapp/frontend/src/constants/URL_CONSTANTS.js
@@ -1,5 +1,5 @@
 // const BACKEND_URL=import.meta.env.VITE_BACKEND_URL;
-const BACKEND_URL="http://news.radha-mounika.com"
+const BACKEND_URL="https://news.radha-mounika.com"
 const CREATE_ORDER_ENDPOINT = "/createOrder"; //to create a order using which we open razorpay payment gateway we give email and get order id
 const HANDLE_PAYMENT_ENDPOINT = "/handlePayment"; //once payment gateway is finshed and closed we send the order id payment id ,email address,signature to the backend to verify the payment and send the status message to the frontend
 const URL_CONSTANTS = {
